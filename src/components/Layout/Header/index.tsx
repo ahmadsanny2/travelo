40fd2515574d8419df1817c09de4ab5f74d0b2bf
ahmadsanny2 <ahmadsanijabarulloh.02@gmail.com>
2025-09-10 02:41:47 +0700

    Create container component

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,3 +1,4 @@
+import Container from 'components/Container';
 import Image from 'next/image';
 
 import traveloLogo from "public/assets/travelo-logo-header.png"
@@ -5,9 +6,11 @@ import traveloLogo from "public/assets/travelo-logo-header.png"
 const Header = () => {
     return (
         <header className="h-[85px] bg-white shadow-lg p-4">
-            <div className="flex justify-between items-center">
-                <Image src={traveloLogo} width="182" height="48" alt="Travelo Logo" placeholder='blur' />
-            </div>
+            <Container>
+                <div className="flex justify-between items-center">
+                    <Image src={traveloLogo} width="182" height="48" alt="Travelo Logo" placeholder='blur' />
+                </div>
+            </Container>
         </header>
     )
 }
