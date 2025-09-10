393cc49a2737e7edd48c5fa86671f1bc650e4f9a
ahmadsanny2 <ahmadsanijabarulloh.02@gmail.com>
2025-09-11 03:04:50 +0700

    Create SearchComponent

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 
 import traveloLogo from "public/assets/travelo-logo-header.png"
 import Nav from './Nav';
+import Search from './Search';
 
 const Header = () => {
     return (
@@ -11,6 +12,7 @@ const Header = () => {
                 <div className="flex justify-between items-center">
                     <div className="flex">
                         <Image src={traveloLogo} width="182" height="48" alt="Travelo Logo" placeholder='blur' />
+                        <Search />
                     </div>
 
                     <div className="flex">
