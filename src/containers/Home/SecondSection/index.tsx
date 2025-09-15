5a7133088a96ed6d56057269834f8bc1d9b86b7f
ahmadsanny2 <ahmadsanijabarulloh.02@gmail.com>
2025-09-15 19:56:29 +0700

    Create secondSection Component for HomePage

--- src/containers/Home/SecondSection/index.tsx
+++ src/containers/Home/SecondSection/index.tsx
@@ -1,6 +1,13 @@
+import Container from 'components/Container'
+import styles from './SecondSection.module.css'
+
 const SecondSection = () => {
     return (
-        <div className=""></div>
+        <section className={styles.section}>
+            <Container>
+
+            </Container>
+        </section>
     )
 }
 
