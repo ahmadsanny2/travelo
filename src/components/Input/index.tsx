1c2272f8b88015c6e5dc1fb741bb7a40331cd090
ahmadsanny2 <ahmadsanijabarulloh.02@gmail.com>
2025-10-04 09:48:48 +0700

    Improving inputComponent

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,9 +5,21 @@ import { useState } from "react"
 import { FaEye, FaEyeSlash } from "react-icons/fa"
 
 
-const Input = ({ name, type, label, placeholder, className }: InputProps) => {
+const Input = ({
+    name,
+    type,
+    label,
+    placeholder,
+    className,
+    register,
+    errors,
+    required,
+    pattern
+}: InputProps) => {
     const [hidePassword, setHidePassword] = useState(false)
     const [inputType, setInputType] = useState(type)
+    const isInputError = errors && errors[name]
+    const inputErrorHint = `${name}-input-error-hint`
 
     const handleEyeClick = () => {
         setHidePassword((prevState) => !prevState)
@@ -25,15 +37,20 @@ const Input = ({ name, type, label, placeholder, className }: InputProps) => {
             <div className="mb-3">{label}</div>
 
             <input
+                {...register(name, {
+                    required,
+                    pattern
+                })}
+                formNoValidate
                 type={inputType}
-                name={name}
                 id={name}
                 placeholder={placeholder}
                 className={styles.input}
+                aria-describedby={inputErrorHint}
             />
 
             {type === "password" ? (
-                <button type="button" className={styles.eye} onClick={handleEyeClick}>
+                <button type="button" className={clsx(styles.eye, isInputError && styles.eyeError)} onClick={handleEyeClick}>
                     {hidePassword ? (
                         <FaEyeSlash className="h-6 w-6 text-gray-100" />
                     ) : (
@@ -42,6 +59,11 @@ const Input = ({ name, type, label, placeholder, className }: InputProps) => {
                 </button>
             ) : null}
 
+            {isInputError ? (
+                <span id={inputErrorHint} className="text-xs text-red-100 mt-1 ml-3">
+                    {errors[name].message}
+                </span>
+            ) : null}
         </label>
     )
 }
