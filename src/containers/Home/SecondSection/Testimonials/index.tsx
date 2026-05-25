376c41a7dd619899a714f07e15430b33b548aad4
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-05-25 11:02:05 +0700

    fix(home): reduce testimonial count to three to match grid columns layout

--- src/containers/Home/SecondSection/Testimonials/index.tsx
+++ src/containers/Home/SecondSection/Testimonials/index.tsx
@@ -3,13 +3,13 @@ import TestimonialCard from "./TestimonialCard"
 const Testimonials = () => {
     return (
         <div className="px-5">
-            <h3 className="text-heading-3 font-label font-bold pt-14 mb-1">
+            <h3 className="mb-1 font-bold text-heading-3 font-label pt-14">
                 Testimonials Pelanggan
             </h3>
-            <p className="text-heading-4 mb-4">Pendapat pelanggan tentang kami</p>
+            <p className="mb-4 text-heading-4">Pendapat pelanggan tentang kami</p>
 
-            <div className="grid lg:grid-cols-3 gap-10 justify-between">
-                {Array.from({ length: 4 }).map((_, index) => {
+            <div className="grid justify-between gap-10 lg:grid-cols-3">
+                {Array.from({ length: 3 }).map((_, index) => {
                     return (
                         <TestimonialCard key={index}/>
                     )
